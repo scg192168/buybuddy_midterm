4fcad5505e34cf8d30a8b0613d38315d7899a738
Shyamal <scghoshbc@gmail.com>
2023-09-17 15:54:01 -0700

    incorporated users.sql + routes + css

--- server.js
+++ server.js
@@ -2,11 +2,9 @@
 require("dotenv").config();
 
 // Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
-const cookieSession = require("cookie-session");
-
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -18,8 +16,8 @@ app.set("view engine", "ejs");
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/styles',
+app.use(
+  "/styles",
   sassMiddleware({
     source: __dirname + "/styles",
     destination: __dirname + "/public/styles",
@@ -30,15 +28,15 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require("./routes/users-api");
-const widgetApiRoutes = require("./routes/widgets-api");
+//const userApiRoutes = require("./routes/users-api");
+//const widgetApiRoutes = require("./routes/widgets-api");
 const usersRoutes = require("./routes/users");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use("/api/users", userApiRoutes);
-app.use("/api/widgets", widgetApiRoutes);
+//app.use("/api/users", userApiRoutes);
+//app.use("/api/widgets", widgetApiRoutes);
 app.use("/users", usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
@@ -49,15 +47,6 @@ app.use("/users", usersRoutes);
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-// /products/endpoints
-app.use("/products", productRoutes);
-
-// /user/endpoints
-app.use("/users", userRoutes);
-
-// /api/endpoints
-// app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
