ff89128449119ac85bb32848bf24fe6fc635194e
Hosu <sogeumhosu@gmail.com>
2023-09-17 08:42:44 -0700

    Configured routes and templates for login and registration pages

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,19 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+router.get("/", (req, res) => {
+  res.render("users");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 module.exports = router;
