77bec5018125f2be099ec6c6d7a5a2986eaa1930
Shyamal <scghoshbc@gmail.com>
2023-09-17 21:04:55 -0700

    product.ejs + products.js + server files updated

---
+++ routes/products.js
@@ -0,0 +1,14 @@
+// routes/products.js
+const express = require("express");
+const router = express.Router();
+const db = require("../db"); // Import your database connection or queries module
+
+router.get("/", (req, res) => {
+  // Fetch products from your database (replace with actual code)
+  const products = db.getProducts(); // Implement this function
+
+  // Render the products template and pass the products data
+  res.render("products", { products });
+});
+
+module.exports = router;
